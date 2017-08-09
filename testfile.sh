40f19707227c7f917da5a22a828ddbe69f2d2b38
Prasad BG <ezeetester@users.noreply.github.com>
2017-08-09 23:37:46 +0530

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -3,5 +3,5 @@ cd testfolder
 touch docfile.txt
 uname -a >> docfile.txt
 firefox -v >> docfile.txt
-byzanz-record -d 30 --delay=5 /home/prasad/GitAutomatedBugRaiser/testfolder/byzanz-testthree.webm
+byzanz-record -d 30 --delay=5 /path/tothefoldercontainingtherecordedvideo/videofilename.webm
 
