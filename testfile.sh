ff6607659261b19b193e14d1b582bfda93deafd4
Prasad BG <ezeetester@users.noreply.github.com>
2017-09-25 23:06:31 +0530

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -1,7 +1,7 @@
 mkdir -p testfolder
 cd testfolder
 touch docfile.txt
-uname -a >> docfile.txt
-firefox -v >> docfile.txt
+uname -a > docfile.txt
+firefox -v > docfile.txt
 byzanz-record -d 30 --delay=5 /path/tothefoldercontainingtherecordedvideo/videofilename.webm
 
