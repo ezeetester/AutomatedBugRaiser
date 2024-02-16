0a80c1130b3e835b3553b333ce632d55518a7c52
Guruprasad B Gopinaath <ezeetester@users.noreply.github.com>
2024-02-16 13:11:33 +0530

    Update testfile.sh
    Changed the redirection operator to append.

--- testfile.sh
+++ testfile.sh
@@ -2,6 +2,6 @@ mkdir -p testfolder
 cd testfolder
 touch docfile.txt
 uname -a > docfile.txt
-firefox -v > docfile.txt
+firefox -v >> docfile.txt
 byzanz-record -d 30 --delay=5 /path/tothefoldercontainingtherecordedvideo/videofilename.webm
 
